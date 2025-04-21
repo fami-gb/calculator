20b208c915d5c5e5b5959575d005c8adb6413e04
fami-gb <fami.gb0825@gmail.com>
2025-04-21 22:08:39 +0900

    chore:計算履歴を保存する機能をアップデート

--- client/server.js
+++ client/server.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const fetchBtn = document.getElementById('btn_eval');
     fetchBtn.addEventListener('click', async () => {
         const display = document.getElementsByClassName('display')[0];
+        const ulElement = document.getElementsByClassName('history-container')[0];
+        ulElement.className = 'history-container';
         const calcResult = display.value;
         try {
             const response = await fetch('http://localhost:8000/api/data', {
@@ -9,9 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ result: calcResult })
             });
-            const data = await response.json();
             if (response.ok) {
-                // 計算機の下に送信成功メッセージを表示
+                const data = await response.json();
+                ulElement.innerHTML = '';
+                data.forEach((c) => {
+                    const liElement = document.createElement('li');
+                    liElement.className = 'history-item';
+                    liElement.innerHTML = c.result + `<span class="calc-date">\
+                    [${c.date}]</span>`;
+                    ulElement.appendChild(liElement);
+                });
             } else {
                 console.error("Error evaluating expression:", data.error);
             }
@@ -19,4 +28,5 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error("Network error:", error);
         }
     });
+
 });
--- server/server.js
+++ server/server.js
@@ -3,21 +3,26 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-let data = "";
+const data = [];
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/api/data', (req, res) => {
-    data = req.body.result;
-    return res.status(200).json({ message: '計算結果を取得しました。'});
+    if (!req.body.result) { return res.status(400).json({ message: '計算結果が見つかりません。' }) }
+    const newData = {
+        result: req.body.result,
+        date: new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })
+    };
+    data.push(newData);
+    return res.status(200).json(data);
 });
 
 app.get('/api/data', (req, res) => {
     if (!data) { return res.status(400).json({ message: '計算結果が見つかりません。' }) }
-    return res.status(200).json( { result: data });
+    return res.status(200).json(data);
 });
 
 app.listen(8000, () => {
     console.log('Server is running on http://localhost:8000');
-})
+});
